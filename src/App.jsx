164f776494f8dc9cb5fcf70a32659adf9ef06803
Aditya Kumar Samantaray <adityasamantaray985@gmail.com>
2024-07-26 13:23:23 +0530

    Features Section Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import Features from "./components/Features";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Hero />
         <Highlights />
         <Model />
+        <Features />
       </main>
     </>
   );
