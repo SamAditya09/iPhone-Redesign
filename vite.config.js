598b0f8816c6ce29033b7e3bd85a8981685c6598
Aditya Kumar Samantaray <adityasamantaray985@gmail.com>
2024-07-27 22:00:42 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/iPhone-Redesign/"
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public',
+  base: "/iPhone-Redesign/",
 });
